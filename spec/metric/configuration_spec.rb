c54a7ab04ab2d282833a64ac62aa8551c7978ce8
Mark Mulder <markmulder@gmail.com>
2011-09-08 10:43:50 +0200

    Cleanup configuration spec

--- spec/metric/configuration_spec.rb
+++ spec/metric/configuration_spec.rb
@@ -8,13 +8,6 @@ describe Metric::Configuration do
     Metric.configuration.metric_host.should == "http://api.metric.io"
   end
 
-  it "configures api_key" do
-    Metric.configure do |config|
-      config.api_key = "test"
-    end
-    Metric.configuration.api_key.should == "test"
-  end
-
   it "configures metric host" do
     Metric.configure do |config|
       config.metric_host = "http://localhost:5000"
@@ -22,7 +15,14 @@ describe Metric::Configuration do
     Metric.configuration.metric_host.should == "http://localhost:5000"
   end
 
-  it "configures secret token" do
+  it "configures api_key" do
+    Metric.configure do |config|
+      config.api_key = "test"
+    end
+    Metric.configuration.api_key.should == "test"
+  end
+
+  it "configures secret key" do
     Metric.configure do |config|
       config.secret_key = "random_string"
     end
